9985016bcf90d9d57897fe1872fbb4dc135cc078
unknown <milica.milica.b@gmail.com>
2019-01-12 17:14:24 +0100

    Kupovina naucnog rada

--- src/app/naucni-radovi/naucni-radovi.component.ts
+++ src/app/naucni-radovi/naucni-radovi.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { NaucnaCentralaService } from '../naucna-centrala.service';
+import { Constants } from '../constants/constants';
 
 @Component({
   selector: 'app-naucni-radovi',
@@ -11,6 +12,7 @@ export class NaucniRadoviComponent implements OnInit {
   
   izdanjeId : number;
   radovi: any;
+  korisnik: any;
 
   constructor(private route: ActivatedRoute, public ncService : NaucnaCentralaService, private router: Router) { }
 
@@ -20,10 +22,22 @@ export class NaucniRadoviComponent implements OnInit {
     
     this.izdanjeId = parseInt(this.route.snapshot.paramMap.get('izdanjeId'));
 
+    this.ncService.getActiveUser().subscribe(data =>{
+      this.korisnik = data;
+    })
+
     this.ncService.izlistajSveRadove(this.izdanjeId).subscribe(data=>{
       console.log(data);
       this.radovi = data;
     })
+  }
+
+  kupiRad(id, cena) {
+    this.ncService.executePayment(id, Constants.TIP_PROIZVODA_NAUCNI_RAD, this.korisnik.id, cena).subscribe(data=> {
+      console.log(data);
+      window.open(data);
+    })
+   
   }
 
 }
